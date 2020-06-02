26f4057ca9986f43676fd5c14aba087757e46452
Manish Poduval <manishpoduval@hotmail.com>
2020-06-02 18:08:22 +0530

    Authentication final

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ require('./configs/db.config');
 
 // Routers
 const indexRouter = require('./routes/index.routes');
+const authRouter = require('./routes/auth.routes');
 
 const app = express();
 
@@ -32,6 +33,26 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // Sets up morgan in our middleware so that we can see the requests getting logged
 app.use(logger('dev'));
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+ 
+app.use(session({
+    secret: 'my-secret-weapon',
+    saveUninitialized: false,
+    resave: true,
+    cookie: {
+      maxAge: 60*60*24*1000 //60 sec * 60 min * 24hrs = 1 day (in milliseconds)
+    },
+    store: new MongoStore({
+        url: 'mongodb://localhost/basicAuth',
+        // mongooseConnection: mongoose.connection
+        //time to live (in seconds)
+        ttl: 60*60*24,
+        autoRemove: 'disabled'
+    })
+}));
+
+
 // a body parser to allow us to parse form submissions
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -41,5 +62,6 @@ app.use(cookieParser());
 
 // Routes middleware
 app.use('/', indexRouter);
+app.use('/', authRouter);
 
 module.exports = app;
--- models/User.model.js
+++ models/User.model.js
@@ -2,7 +2,20 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
   {
-    /*Define schema here */
+     username: {
+       type: String,
+       required: [true, 'Please enter username'], 
+       unique: true
+     }, 
+     email: {
+      type: String,
+      required: [true, 'Please enter email'],
+      unique: [true, 'Email registered. Please use a new one']
+    },
+     passwordHash: {
+      type: String,
+      required: true
+    }
   },
   {
     timestamps: true
