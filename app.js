c2c642ce6019d124fd5436fd9d275ae596512100
James Dawson <63356869+polymurph13@users.noreply.github.com>
2020-06-03 08:35:34 +0100

    stuff

--- app.js
+++ app.js
@@ -36,21 +36,21 @@ app.use(logger('dev'));
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
  
-app.use(session({
-    secret: 'my-secret-weapon',
-    saveUninitialized: false,
-    resave: true,
-    cookie: {
-      maxAge: 60*60*24*1000 //60 sec * 60 min * 24hrs = 1 day (in milliseconds)
-    },
-    store: new MongoStore({
-        url: 'mongodb://localhost/basicAuth',
-        // mongooseConnection: mongoose.connection
-        //time to live (in seconds)
-        ttl: 60*60*24,
-        autoRemove: 'disabled'
-    })
-}));
+// app.use(session({
+//     secret: 'my-secret-weapon',
+//     saveUninitialized: false,
+//     resave: true,
+//     cookie: {
+//       maxAge: 60*60*24*1000 //60 sec * 60 min * 24hrs = 1 day (in milliseconds)
+//     },
+//     store: new MongoStore({
+//         url: 'mongodb://localhost/basicAuth',
+//         // mongooseConnection: mongoose.connection
+//         //time to live (in seconds)
+//         ttl: 60*60*24,
+//         autoRemove: 'disabled'
+//     })
+// }));
 
 
 // a body parser to allow us to parse form submissions
